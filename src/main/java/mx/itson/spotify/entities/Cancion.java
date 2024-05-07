53ad4d91537dbde49196e96f3ca1970e42c394d7
980024653 <980024653@10.232.15.87>
2024-05-07 12:12:30 -0700

    Versión inicial del proyecto

---
+++ src/main/java/mx/itson/spotify/entities/Cancion.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.spotify.entities;
+
+/**
+ *
+ * @author Luis Morell
+ */
+public class Cancion {
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the duracion
+     */
+    public int getDuracion() {
+        return duracion;
+    }
+
+    /**
+     * @param duracion Duracion de la canción expresada en segundos
+     */
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+    
+    private String titulo;
+    private int duracion;
+    
+}
---
+++ src/main/java/mx/itson/spotify/ui/Spotify.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.spotify.ui;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import mx.itson.spotify.entities.Album;
+import mx.itson.spotify.entities.Artista;
+import mx.itson.spotify.entities.Cancion;
+
+/**
+ *
+ * @author Luis Morell
+ */
+public class Spotify {
+    
+    public static void main(String[] args) {
+        
+        Artista artista = new Artista();
+        artista.setNombre("Ezkai");
+        artista.setPerfil("Artista versátil");
+        artista.setOyentesMensuales(100000);
+        artista.setFollowers(550000);
+     
+        Cancion cancion1 = new Cancion();
+        cancion1.setTitulo("Lo Minimo");
+        cancion1.setDuracion(180);
+        
+        Cancion cancion2 = new Cancion();
+        cancion2.setTitulo("Villain");
+        cancion2.setDuracion(200);
+        
+        List<Cancion> temas = new ArrayList<>();
+        temas.add(cancion1);
+        temas.add(cancion2);
+        
+        Album album = new Album();
+        album.setNombre("Rain");
+        album.setArtista(artista);
+        album.setFechaLanzamiento(new Date());
+        album.setCanciones(temas);
+        album.setGenero("Pop");
+        
+        System.out.println(album.getArtista().getNombre());
+        System.out.println(album.getArtista().getPerfil());
+        System.out.println(album.getArtista().getOyentesMensuales());
+        System.out.println(album.getArtista().getFollowers());
+        
+        
+    }
+}
